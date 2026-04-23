db5fb9fd78c7f81d2188b0f2b2ff168bc80d8324
Amelia08-14 <a.benelhadj@aymenpromotion.com>
2026-04-23 10:42:39 +0100

    login problem

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const { login, changePassword } = require('../controllers/authController');
+const { login, changePassword, me } = require('../controllers/authController');
 const { protect } = require('../middleware/authMiddleware');
 
 router.post('/login', login);
+router.get('/me', protect, me);
 router.put('/password', protect, changePassword);
 
 module.exports = router;
