95a95603fd51eabaa152d8033b0d42dfcc949e9e
Amelia08-14 <a.benelhadj@aymenpromotion.com>
2026-05-05 19:03:18 +0100

    appele de fond et ticket 4

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -8,26 +8,29 @@ const protect = async (req, res, next) => {
     req.headers.authorization &&
     req.headers.authorization.startsWith('Bearer')
   ) {
-    try {
-      token = req.headers.authorization.split(' ')[1];
+    token = req.headers.authorization.split(' ')[1];
+    if (!token) {
+      return res.status(401).json({ error: 'Not authorized, no token' });
+    }
 
+    try {
       const decoded = jwt.verify(token, process.env.JWT_SECRET || 'secret');
 
       req.user = await User.findByPk(decoded.id);
 
       if (!req.user) {
-         return res.status(401).json({ error: 'Not authorized, user not found' });
+        return res.status(401).json({ error: 'Not authorized, user not found' });
       }
 
-      next();
+      return next();
     } catch (error) {
       console.error(error);
-      res.status(401).json({ error: 'Not authorized, token failed' });
+      return res.status(401).json({ error: 'Not authorized, token failed' });
     }
   }
 
   if (!token) {
-    res.status(401).json({ error: 'Not authorized, no token' });
+    return res.status(401).json({ error: 'Not authorized, no token' });
   }
 };
 
