07480d486a1f587f08015e34aef6b1d04101ddd9
Amelia08-14 <a.benelhadj@aymenpromotion.com>
2026-08-24 21:54:09 +0100

    finalisation

--- routes/messageRoutes.js
+++ routes/messageRoutes.js
@@ -1,20 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const { protect } = require('../middleware/authMiddleware');
-const {
-  getTicketMessages,
-  sendTicketMessage,
-  getAdminMessages,
-  sendAdminMessage,
-  getAdminThreads,
-  markAdminThreadRead,
-} = require('../controllers/messageController');
+const { getTicketMessages, sendTicketMessage } = require('../controllers/messageController');
 
 router.get('/ticket/:ticketId', protect, getTicketMessages);
 router.post('/ticket/:ticketId', protect, sendTicketMessage);
-router.get('/admin/threads', protect, getAdminThreads);
-router.put('/admin/:userId/read', protect, markAdminThreadRead);
-router.get('/admin', protect, getAdminMessages);
-router.post('/admin', protect, sendAdminMessage);
 
 module.exports = router;
